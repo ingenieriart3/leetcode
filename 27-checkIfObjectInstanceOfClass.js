6478d5c39d55d4bda7afa86f60d1971f2fdfcb7b
IA3 <ingenieriart3@gmail.com>
2024-08-19 21:10:35 -0300

    finish js 30

--- 27-checkIfObjectInstanceOfClass.js
+++ 27-checkIfObjectInstanceOfClass.js
@@ -54,3 +54,29 @@ checkIfInstanceOf(new Date(), Date); // true
 // Input: func = () => checkIfInstanceOf(5, Number)
 // Output: true
 // Explanation: 5 is a Number. Note that the "instanceof" keyword would return false. However, it is still considered an instance of Number because it accesses the Number methods. For example "toFixed()".
+
+const d = new Date();
+
+console.log(Date.prototype); // shows object if it's on the browser
+console.log(Object.getPrototypeOf(d)); //shows object if it's on nodejs thread
+
+class DateSubclass extends Date {}
+
+const d2 = new DateSubclass();
+const d2Proto = Object.getPrototypeOf(d2);
+console.log(Object.getPrototypeOf(d2Proto));
+
+let n1 = 1;
+let n2 = new Number(1);
+
+console.log(n1 instanceof Number); // false
+console.log(n2 instanceof Number); // true
+
+let s1 = '1';
+let s2 = new String('1');
+
+console.log(s1 instanceof String); // false
+console.log(s2 instanceof String); // true
+
+console.log(Object.getPrototypeOf(s1));
+console.log(Object.getPrototypeOf(s2));
